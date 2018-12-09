df04861f95337fcd2ce0d71a2c81897e86636676
Julius camarillo <julcamarillo17@gmail.com>
2018-12-09 16:30:22 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,20 @@
 
 var cards = ["queen","queen","king","king"];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+
+var checkForMatch = function (){
 if (cardsInPlay.length === 2){
 	if(cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!")
+		alert("You found a match!");
 	} else{alert("Sorry, try again!")}
 }
+};
+
+var flipCard = function (cardId){
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped" + " " + cards[cardId])
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
